05839a7c3f168f11e79fa57a485b3c88c0f141b1
Amina Djarfi <m.djarfi.amina@gmail.com>
2024-03-02 19:24:11 +0100

    optimisation

--- src/data_prepare.py
+++ src/data_prepare.py
@@ -1,38 +1,37 @@
+import argparse
 import os
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
+__all__ = ['preprocess_data']
 
-def main():
-    # data directory
-    data_dir = '../data/'
 
-    # create data directory if not exists
-    if not os.path.exists(data_dir):
-        os.makedirs(data_dir)
+def preprocess_data(data_dir, corpus_path, macrolanguages_path, labels, test_split=0.2):
+    """Prétraitement des données
 
-    # download the corpus
-    corpus_path = data_dir + 'sentences.tar.bz2'
+    Args:
+        data_dir (str): Répertoire de sauvegarde des données
+        corpus_path (str): Chemin du fichier contenant les données
+        macrolanguages_path (str): Chemin du fichier contenant les langues
+        labels (list): Liste des langues à garder
+        test_split (float): Proportion des données à garder pour le test
 
-    # download the macrolanguages
-    macrolanguages_path = data_dir + 'iso-639-3-macrolanguages.tab'
+    Returns:
+        train_data (pd.DataFrame): Données d'entraînement
+        test_data (pd.DataFrame): Données de test
 
-    # languages list
-    labels = {'eng': 'English', 'deu': 'German', 'fra': 'French', 'spa': 'Spanish', 'ita': 'Italian',
-              'nor': 'Norwegian',
-              'tur': 'Turkish',
-              'rus': 'Russian',
-              'ell': 'Greek', 'dan': 'Danish', 'fin': 'Finnish',
-              'ara': 'Arabic', 'heb': 'Hebrew', 'zho': 'Chinese', 'hin': 'Hindi', 'jpn': 'Japanese', 'fas': 'Persian',
-              'kor': 'Korean',
-              'lat': 'Latin',
-              'vie': 'Vietnamese', 'tha': 'Thai'}
+        """
+
+    # Vérifier si les fichiers existent
+    if not os.path.exists(corpus_path) or not os.path.exists(macrolanguages_path):
+        print("Un ou plusieurs fichiers sont manquants.")
+        return
 
     # Chargement du corpus
     print(f"Load corpus from file {corpus_path}...")
-    data = pd.read_csv(corpus_path, sep='\t',
-                       header=None, compression='bz2', low_memory=False)
+
+    data = pd.read_csv(corpus_path, sep='\t', header=None, compression='bz2', low_memory=False)
     data.columns = ['id', 'I_Id', 'content']
 
     # suppression des valeurs NAN
@@ -48,8 +47,8 @@ def main():
     # jointure entre la table des sentences et la table macrolanguages
     data = pd.merge(data, macro_lang, how='left', on='I_Id')
 
-    # remplacer les valeurs NAN dans la colonne 'lang' par la valeur de la colonne 'I_Id'
-    data.fillna({'label': data.I_Id}, inplace=True)
+    # Remplacer les valeurs NaN dans la colonne 'lang' par la valeur de la colonne 'I_Id'
+    data['label'] = data['label'].fillna(data['I_Id'])
 
     # suppression des columns 'I_Status', 'id' et 'I_Id'
     data = data.drop(columns=['I_Status', 'id', 'I_Id'])
@@ -60,8 +59,6 @@ def main():
     # suppression des doublons
     data = data[~data["content"].duplicated()]
 
-
-
     #
     # Rééchantillonnage des données
     #
@@ -69,7 +66,6 @@ def main():
     # Étape 1: Identifier les labels avec plus de 100000 lignes
     #
     print("Rééchantillonnage des données...")
-
 
     # Compter le nombre de lignes par label après le filtrage initial
     label_counts = data['label'].value_counts()
@@ -83,8 +79,6 @@ def main():
     #
     # Étape 2: Filtrer les lignes avec moins de 4 tokens uniquement pour ces labels
     #
-
-
 
     # Liste des labels à exclure du filtrage
     labels_to_exclude = ['jpn', 'zho', 'hin', 'kor', 'vie', 'tha']
@@ -111,7 +105,6 @@ def main():
     # Concaténer les trois ensembles pour obtenir le dataframe final
     df_final = pd.concat([df_filtered, df_excluded_labels, df_under_100k], axis=0)
 
-
     #
     # Étape 4: Limiter à 100000 lignes pour les labels filtrés
     #
@@ -123,19 +116,15 @@ def main():
 
     # Limiter à 100000 lignes pour chaque label concerné
     for label in labels_over_nrows:
-
         df_temp = df_final[df_final['label'] == label].sample(n=100000, random_state=42)
         data_final = pd.concat([data_final, df_temp], axis=0)
 
     # Ajouter les lignes des labels non concernés par la limitation
     data_final = pd.concat([data_final, df_final[~df_final['label'].isin(labels_over_nrows)]], axis=0)
 
-
     print(f"Final data shape: {data_final.shape}")
 
     # Sauvegarder le résultat dans un fichier
-
-
 
     test_split = 0.2
 
@@ -145,16 +134,47 @@ def main():
         test_size=test_split,
         stratify=data_final['label'].values,
     )
-
-    print(f"save result in '{data_dir}*_data.pkl'...")
-
-    train_data.to_pickle(data_dir + 'train_data.pkl')
-    test_data.to_pickle(data_dir + 'test_data.pkl')
-
-    print(train_data.describe())
-
     print(f"Preprocessing end...")
+
+    # Sauvegarder les résultats dans des fichiers
+    print(f"Sauvegarde des résultats dans '{data_dir}*_data.pkl'...")
+    train_data.to_pickle(os.path.join(data_dir, 'train_data.pkl'))
+    test_data.to_pickle(os.path.join(data_dir, 'test_data.pkl'))
+
+    return train_data, test_data
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Preprocess data")
+    parser.add_argument('--data_dir', type=str, default='../data/', help="Répertoire de sauvegarde des données")
+    parser.add_argument('--corpus_path', type=str, default='../data/sentences.tar.bz2',
+                        help="Chemin du fichier contenant les données")
+    parser.add_argument('--macrolanguages_path', type=str, default='../data/iso-639-3-macrolanguages.tab',
+                        help="Chemin du fichier contenant les langues")
+    parser.add_argument('--test_split', type=float, default=0.2, help="Proportion des données à garder pour le test")
+
+    args = parser.parse_args()
+
+    # Création du répertoire des données s'il n'existe pas
+
+    # Répertoire des données
+    data_dir = args.data_dir
+
+    # Chemin des fichiers
+    corpus_path = args.corpus_path
+    macrolanguages_path = args.macrolanguages_path
+
+    # Création du répertoire des données s'il n'existe pas
+    os.makedirs(data_dir, exist_ok=True)
+
+    labels = {'eng': 'English', 'deu': 'German', 'fra': 'French', 'spa': 'Spanish', 'ita': 'Italian',
+              'nor': 'Norwegian',
+              'tur': 'Turkish',
+              'rus': 'Russian',
+              'ell': 'Greek', 'dan': 'Danish', 'fin': 'Finnish',
+              'ara': 'Arabic', 'heb': 'Hebrew', 'zho': 'Chinese', 'hin': 'Hindi', 'jpn': 'Japanese', 'fas': 'Persian',
+              'kor': 'Korean',
+              'lat': 'Latin',
+              'vie': 'Vietnamese', 'tha': 'Thai'}
+
+    train_data, test_data = preprocess_data(data_dir, corpus_path, macrolanguages_path, labels)
